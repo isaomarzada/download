eca3571bcc82d6a083ef75a8b92d5264379634b8
Isa Omarzada <isaomarzada@yahoo.com>
2019-07-29 23:37:09 -0500

    heroku

---
+++ src/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app     = express();
+const path    = require('path');
+const port    = 3000;
+
+app.use(express.static('src'));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/download.html')))
+
+app.listen(process.env.PORT || port, () => console.log(`App listening on port ${port}!`));
